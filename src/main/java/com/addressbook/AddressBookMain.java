5dcfdc82bbebba5a4131d7bfb67c09cf85326baf
sngtmth <sngtmth@gmail.com>
2021-11-17 08:30:27 +0530

    Added:Welcome Message for Address Book System

---
+++ src/main/java/com/addressbook/AddressBookMain.java
@@ -0,0 +1,11 @@
+/**
+ * Address Book System
+ */
+package com.addressbook;
+
+public class AddressBookMain {
+    //Main Method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book System");
+    }
+}
